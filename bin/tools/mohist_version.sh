1b1d695a2f2de7e16681716effd3e87f4567d85d
MemoryShadow <MemoryShadow@outlook.com>
2022-04-27 21:42:43 +0800

    Add: mohist downlaod link query shell file
    Add: mohist downlaod link query shell file
    alter: optimization paper downlaod link query shell file

---
+++ bin/tools/mohist_version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo The parameter does not exist, please pass in the version number to be queried;
+  echo
+  echo -e "${0} <Version|latest> [-q]"
+  echo -e "    Version\tMinecraft server file version"
+  exit 1;
+fi;
+
+declare -A versionList=(
+  ['1.7.1']=1.7.10
+  ['1.7.10']=1.7.10
+  ['1.12.2']=1.12.2
+  ['1.16.5']=1.16.5
+  ['1.18.2']=1.18.2
+  ['1.18.2-testing']=1.18.2
+)
+if [ -z ${versionList[$1]} ]; then
+  echo "Version does not exist, script has exited";exit 1;
+else
+  URL=`curl -s https://mohistmc.com/api/${versionList[$1]}/latest`
+  URL=${URL%\",*};URL=${URL%\",*};echo ${URL##*:\"};exit 0;
+fi;
--- bin/tools/paper_version.sh
+++ bin/tools/paper_version.sh
@@ -1,48 +1,24 @@
 #!/bin/bash
 if [ -z "$1" ]; then
-  echo 参数不存在，请传入需要查询的版本号;
+  echo The parameter does not exist, please pass in the version number to be queried;
   echo
   echo -e "${0} <Version|latest> [-q]"
   echo -e "    Version\tMinecraft server file version"
-  echo -e "    -q\t\tOpen quiet mode:Only show the URL of the latest version of the current query"
   exit 1;
 fi;
-# 检查：只要参数列表中带有-q，就打开开关
-Quiet_F=0
-for item in $*
-do
-  if [ "${item}" == "-q" ]; then
-    Quiet_F=1;
-    break;
-  fi
-done
 version=$1
-# 校验版本是否为关键字，如果是，就自动查询最新版本
+# Check whether the version is a keyword, if so, automatically query the latest version
 if [ "$1" == "latest" ]; then
   version=`curl -s https://papermc.io/api/v2/projects/paper`;version=${version##*,\"};version=${version%%\"*}
+  else
+  # Check if the version exists
+  curl -s https://papermc.io/api/v2/projects/paper | grep "${version}" > /dev/null
 fi
-if [ ${Quiet_F} == 0 ]; then
-  echo ================
-  echo 查询目标: paper
-  echo 查询版本: ${version}
-  echo 检查版本是否存在....
-fi
-# 校验版本是否存在
-curl -s https://papermc.io/api/v2/projects/paper | grep "${version}" > /dev/null
 # (0)
-if [ $? != 0 ]; then echo 版本不存在,脚本已退出;exit 1;fi
-if [ ${Quiet_F} == 0 ]; then echo 版本存在，正在查询最新版本...;fi
-# 获取最新构建名称
+if [ $? != 0 ]; then echo Version does not exist, script has exited;exit 1;fi
+# Get the latest build name
 build=`curl -s https://papermc.io/api/v2/projects/paper/versions/${version}`;build=${build##*,};build=${build%%]*};
-if [ ${Quiet_F} == 0 ]; then echo 最新构建的版本为${build},正在拉取文件名...;fi;
-# 拉取文件名
+# Get file name
 filename=`curl -s https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}`;filename=${filename##*application\":\{\"name\":\"};filename=${filename%%\"*}
-if [ ${Quiet_F} == 0 ]; then echo 文件名为: ${filename};fi;
-# 构建URL
-FileURL="https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}/downloads/${filename}"
-if [ ${Quiet_F} == 0 ]; then
-  echo 此版本最新的构建文件下载地址为:${FileURL};
-  echo ================;
-else
-  echo ${FileURL};
-fi
+echo "https://papermc.io/api/v2/projects/paper/versions/${version}/builds/${build}/downloads/${filename}"
+exit 0;
